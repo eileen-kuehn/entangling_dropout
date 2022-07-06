1453f60a18dc73b1665abb93a4ebb2eada2d006c
Eileen Kuehn <eileen.kuehn@kit.edu>
2022-07-06 15:30:21 +0200

    feat: support time and amplitude errors

--- src/entangling_dropout/pipelines/data_processing/nodes.py
+++ src/entangling_dropout/pipelines/data_processing/nodes.py
@@ -1,14 +1,52 @@
-from typing import Tuple
+from enum import Enum
+import logging
+from typing import Tuple, Union
 import numpy as np
 
 
+class NoiseType(Enum):
+    NONE = 0
+    TIME_ERROR = 1
+    AMPLITUDE_ERROR = 2
+
+
+def convert_noise_type(noise_type: Union[NoiseType, str, int]) -> NoiseType:
+    if isinstance(noise_type, NoiseType):
+        return noise_type
+    elif isinstance(noise_type, int):
+        try:
+            return NoiseType(noise_type)
+        except ValueError:
+            pass
+    elif isinstance(noise_type, str):
+        try:
+            return getattr(NoiseType, noise_type)
+        except AttributeError:
+            pass
+    logging.warning(f"Received unknown noise type {noise_type}")
+    return NoiseType.NONE
+
+
 def generate_data(
-    samples: int = 15, noise: float = 1, seed: int = 1337
-) -> Tuple[str, np.ndarray]:
-    rd = np.random.RandomState(seed)
-    x_values = np.linspace(0, 2, samples) * np.pi
-    distance = x_values[1] - x_values[0]
-    x_noise = (rd.random_sample(size=samples) - 0.5) * distance * noise
-    noisy_x_values = x_values + x_noise
-    y_values = np.sin(x_values)
-    return noisy_x_values, y_values
+    samples: int = 15,
+    noise: float = 1,
+    seed: int = 1337,
+    noise_type: NoiseType = NoiseType.TIME_ERROR,
+) -> Tuple[np.ndarray, np.ndarray, float, float]:
+    rd = np.random.default_rng(seed)
+    x_values = np.linspace(-1, 1, samples)
+    y_values = np.sin(x_values * np.pi)
+    # add noise
+    scale_x = scale_y = 1.0
+    if noise_type == NoiseType.TIME_ERROR:
+        distance = np.abs(x_values[1] - x_values[0])
+        x_noise = rd.normal(loc=0, scale=distance * noise, size=samples)
+        x_values = x_values + x_noise
+        scale_x = max(max(x_values), abs(min(x_values)))
+    elif noise_type == NoiseType.AMPLITUDE_ERROR:
+        y_noise = rd.normal(loc=0, scale=noise, size=samples)
+        y_values = y_values + y_noise
+        scale_y = max(max(y_values), abs(min(y_values)))
+    else:
+        logging.warning(f"Not introducing noise for type {noise_type}")
+    return x_values / scale_x, y_values / scale_y, scale_x, scale_y
--- src/entangling_dropout/pipelines/data_processing/pipeline.py
+++ src/entangling_dropout/pipelines/data_processing/pipeline.py
@@ -1,17 +1,23 @@
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
 
-from .nodes import generate_data
+from .nodes import convert_noise_type, generate_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
+                convert_noise_type,
+                inputs="params:noise_type",
+                outputs="noise_type",
+                name="convert_noise_type",
+            ),
+            node(
                 generate_data,
-                inputs=["params:samples", "params:noise", "params:seed"],
-                outputs=["x_values", "y_values"],
+                inputs=["params:samples", "params:noise", "params:seed", "noise_type"],
+                outputs=["x_values", "y_values", "scale_x", "scale_y"],
                 name="generate_data",
-            )
+            ),
         ]
     )
